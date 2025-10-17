266cf3c086e1dde5b321bb169319411f0cb3c992
RitikaRoy72 <ritikaroy7272@gmail.com>
2025-10-17 17:53:13 -0400

    parametereized board size

--- ConwayGameOfLife.py
+++ ConwayGameOfLife.py
@@ -7,33 +7,36 @@ Created on Mon Sep 29 10:13:29 2025
 
 import time
 import os
+import numpy as np
 
 class ConwaysGame:
-    def __init__(self, board):
-        #Initialize variables
+    def __init__(self, board, size):
+        # Initialize variables
         self.board = board
-    
-    def boardINIT(self):
-        """Create initial board state append false to represent initial conditions. 
+        self.size = size
+        pass
+
+    def init_board(self):
+        """Create initial board state append false to represent initial conditions.
         True forms the center three initial starting conditions
         @self.board represent current frame of the board
-        @ n, m are coordinates to reference appending true and false """
-        self.board = []
-        for n in range (0, 5):
-            self.board.append([])
-            for m in range (0, 5):
-                if (n == 1 or n == 2 or n == 3): 
-                    if (m == 2):
-                        self.board[n].append(True)
-                    else: 
-                        self.board[n].append(False)
-                else:
-                    self.board[n].append(False)
+        @ n, m are coordinates to reference appending true and false"""
+        self.board = np.zeros((self.size, self.size))
+        # self.board[1:3, 1:3] = 1  # block
+
+        # beehive
+        # self.board[1, 2:4] = 1
+        # self.board[2, 1] = 1
+        # self.board[3, 2:4] = 1
+        # self.board[2, 4] = 1
+        mid = self.size // 2
+        self.board[1, mid:self.size] = 1
+
         self.draw()
-    
-    def countNeighbors(self):
-        """A method to count the neighbors, 
-        @ newBoard represents the changes made to the original board before reassignment
+
+    def count_neighbors(self):
+        """A method to count the neighbors,
+        @ new_board represents the changes made to the original board before reassignment
         @ surround represents the collection of points around the cell
         @r, v, h, v, h_neigh, and v_heigh all reference the respective points in the cell
         Live cells with 0 or 1 neighbors die due to underpopulation
@@ -41,48 +44,58 @@ class ConwaysGame:
         Dead cells with 2 live neighbors become alive
         BUGS: Error with the appending of live cells, live cells to the lower entrypoint
         """
-      
-        newBoard = self.board.copy()
-        surround = [(0, 1), (1, 0), (0, -1), (-1, 0), (1, 1), (-1, -1), (1, -1), (-1, 1)]
-        for r in range (0, 5):
-            for c in range (0, 5):
+
+        new_board = self.board.copy()
+        surround = [
+            (0, 1),
+            (1, 0),
+            (0, -1),
+            (-1, 0),
+            (1, 1),
+            (-1, -1),
+            (1, -1),
+            (-1, 1),
+        ]
+        
+        for r in range(0, self.size):
+            for c in range(0, self.size):
                 count = 0
                 for h, v in surround:
-                    h_neigh, v_neigh = r+h, c+v
-                    
-                    if 0<= h_neigh < 5 and 0 <= v_neigh < 5 and (newBoard[h_neigh][v_neigh] == 1):
-                        count +=1
-                        
-                if self.board[r][c] == 1 and (count < 2 or count > 3):
-                    newBoard[r][c] == False
+                    h_neigh, v_neigh = r + h, c + v
+
+                    if (
+                        0 <= h_neigh < self.size
+                        and 0 <= v_neigh < 5
+                        and (self.board[h_neigh][v_neigh] == 1)
+                    ):
+                        count += 1
+
+                if self.board[r, c] == 1 and (count < 2 or count > 3):
+                    new_board[r, c] = 0
                 
-                elif self.board[r][c] == 0 and count == 3:
-                    newBoard[r][c] = True
-        self.board = newBoard
-        
-        
+                elif (self.board[r, c] == 0 and (count > 1 or count < 4)):
+                    new_board[r, c] = 1
+
+
+        self.board = new_board
+
     def draw(self):
         """Method to represent board in consol
         TODO: Re-implement as a numpy graphical representation
         """
         os.system("cls")
-        for n in range (0, len(self.board)):
+        for n in range(0, self.size):
             print(self.board[n])
         print("\n")
         time.sleep(1)
         return
-        
-            
+
     def execute(self):
         """A function to drive the playing of the game"""
-        self.boardINIT()
+        self.init_board()
         while True:
-            self.countNeighbors()
+            self.count_neighbors()
             self.draw()
-            
-ConwaysGame([]).execute()
-        
-                
-                
-        
-        
+
+
+ConwaysGame([], 5).execute()
